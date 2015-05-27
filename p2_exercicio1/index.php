d413feffe55ee754d904a315859040670beb3a95
Bruno Nobrega <pinhobruno@hotmail.com>
2015-05-27 00:43:12 -0300

    Adicionando arquivo Projeto 2 - Exercicio 1

---
+++ p2_exercicio1/index.php
@@ -0,0 +1,11 @@
+<?php
+
+$num = array(12,24,53,54,11,27,29,87,67,98,100);
+
+for($i=0;$i<count($num);$i++){
+   echo $num[$i]." - ".
+           $teste = $num[$i] % 2 == 0? $x = "par"."<br/>":$x = "impar"."<br/>"; # Verifica se é par ou impar.
+}
+
+?>
+
